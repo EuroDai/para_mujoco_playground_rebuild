0fc77019ef473cf0953f2d4cecb6670b2f3294c6
EuroDai <daizy25@mails.tsinghua.edu.cn>
2026-05-16 00:27:43 +0800

    fix bugs

--- mujoco_playground/_src/parahand/para_nontendon_fr3/base.py
+++ mujoco_playground/_src/parahand/para_nontendon_fr3/base.py
@@ -6,6 +6,7 @@ from etils import epath
 from ml_collections import config_dict
 import mujoco
 from mujoco import mjx
+from mujoco.mjx._src import support
 import jax
 import jax.numpy as jp
 import numpy as np
@@ -54,56 +55,86 @@ class ParaNontendonFR3Env(mjx_env.MjxEnv):
       geom = data._impl.contact__geom
       geom1 = geom[:, 0]
       geom2 = geom[:, 1]
-      efc_address = data._impl.contact__efc_address[:, 0]
+      efc_address = data._impl.contact__efc_address
       ncon = jp.asarray(data._impl.nacon).reshape(-1)[0]
-      valid = (jp.arange(geom.shape[0]) < ncon) & (efc_address >= 0)
-      frame = data._impl.contact__frame.reshape(-1, 3, 3)
-      dim = data._impl.contact__dim[:, 0]
+      valid = (jp.arange(geom.shape[0]) < ncon) & (efc_address[:, 0] >= 0)
+      frame = data._impl.contact__frame
+      condim = data._impl.contact__dim
       friction = data._impl.contact__friction
       efc_force = data._impl.efc__force
+      is_pyramidal = self.mj_model.opt.cone == mujoco.mjtCone.mjCONE_PYRAMIDAL
+
+      def _decode_contact_force(cid: jax.Array) -> jax.Array:
+        addr_row = efc_address[cid]
+        addr0 = addr_row[0]
+        dim = condim[cid]
+        fric = friction[cid]
+        mat = frame[cid]
+        dtype = efc_force.dtype
+
+        def _invalid():
+          return jp.zeros(3, dtype=dtype)
+
+        def _valid():
+          if is_pyramidal:
+            offsets = jp.arange(10, dtype=addr0.dtype)
+            indices = addr0 + offsets
+            safe_indices = jp.clip(indices, 0, efc_force.shape[0] - 1)
+            pyramid = jp.where(
+                (indices >= 0) & (indices < efc_force.shape[0]),
+                efc_force[safe_indices],
+                0.0,
+            )
+
+            def _dim1():
+              return jp.array([pyramid[0], 0.0, 0.0, 0.0, 0.0, 0.0], dtype=dtype)
+
+            def _dimn():
+              pairs = jp.arange(5)
+              pair_count = 2 * (dim - 1)
+              normal = jp.where(offsets < pair_count, pyramid, 0.0).sum()
+              tangential = (pyramid[2 * pairs] - pyramid[2 * pairs + 1]) * fric[:5]
+              wrench = jp.zeros(6, dtype=dtype)
+              wrench = wrench.at[0].set(normal)
+              wrench = wrench.at[1:].set(
+                  jp.where(pairs < (dim - 1), tangential, 0.0)
+              )
+              return wrench
+
+            wrench_contact = jax.lax.cond(dim == 1, _dim1, _dimn)
+          else:
+            safe_indices = jp.clip(addr_row, 0, efc_force.shape[0] - 1)
+            values = jp.where(
+                (addr_row >= 0) & (addr_row < efc_force.shape[0]),
+                efc_force[safe_indices],
+                0.0,
+            )
+            wrench_contact = jp.where(jp.arange(6) < dim, values, 0.0)
+
+          wrench_world = (wrench_contact.reshape(2, 3) @ mat).reshape(-1)
+          return wrench_world[:3]
+
+        return jax.lax.cond(valid[cid], _valid, _invalid)
+
+      contact_force_vec = jax.vmap(_decode_contact_force)(jp.arange(geom1.shape[0]))
     else:
       contact = data._impl.contact
       geom1 = contact.geom1
       geom2 = contact.geom2
       efc_address = contact.efc_address
       valid = (geom1 >= 0) & (geom2 >= 0) & (efc_address >= 0)
-      frame = contact.frame.reshape(-1, 3, 3)
-      dim = contact.dim
-      friction = contact.friction
-      efc_force = data._impl.efc_force
+
+      contact_ids = jp.arange(geom1.shape[0])
+      contact_wrench = jax.vmap(
+          lambda cid: support.contact_force(
+              self.mjx_model, data, cid, to_world_frame=True
+          )
+      )(contact_ids)
+      contact_force_vec = contact_wrench[:, :3] * valid[:, None]
 
     cube_geom_ids = jp.array([self.mj_model.geom(name).id for name in consts.CUBE_GEOMS])
     geom1_is_cube = jp.any(geom1[:, None] == cube_geom_ids[None, :], axis=1)
     geom2_is_cube = jp.any(geom2[:, None] == cube_geom_ids[None, :], axis=1)
-
-    def _decode_contact_force(cid: int) -> jax.Array:
-      addr = efc_address[cid]
-      condim = dim[cid]
-
-      def _invalid():
-        return jp.zeros(3, dtype=efc_force.dtype)
-
-      def _valid():
-        if self.mj_model.opt.cone == mujoco.mjtCone.mjCONE_PYRAMIDAL:
-          if condim == 1:
-            force_contact = jp.array([efc_force[addr], 0.0, 0.0])
-          else:
-            pyr = efc_force[addr : addr + 2 * (condim - 1)]
-            fri = friction[cid]
-            force_contact = jp.array([
-                pyr[0::2].sum() + pyr[1::2].sum(),
-                (pyr[0::2] - pyr[1::2]) @ fri[: condim - 1],
-                jp.zeros((), dtype=efc_force.dtype),
-            ])
-        else:
-          force_contact = jp.zeros(3, dtype=efc_force.dtype)
-          force_contact = force_contact.at[:condim].set(efc_force[addr : addr + condim])
-
-        return force_contact @ frame[cid]
-
-      return jax.lax.cond((addr >= 0) & valid[cid], _valid, _invalid)
-
-    contact_force_vec = jax.vmap(_decode_contact_force)(jp.arange(geom1.shape[0]))
 
     contacts = []
     for name in consts.FINGERTIP_TACS:
@@ -117,6 +148,7 @@ class ParaNontendonFR3Env(mjx_env.MjxEnv):
 
     return jp.stack(contacts)
 
+  @staticmethod
   def _mat_to_quat(mat: jax.Array) -> jax.Array:
     trace = mat[0, 0] + mat[1, 1] + mat[2, 2]
 
